a3e275064f00eca66dd9b8a7d297fd3f8cf9da4f
Andrew Christensen <andrewpchristensen701@gmail.com>
2019-09-23 10:49:24 -0400

    second commit

--- echo.py
+++ echo.py
@@ -8,31 +8,63 @@ __author__ = "andrewpchristensen701"
 import sys
 import argparse
 
+
+def upper(str):
+    return str.upper()
+
+
+def lower(str):
+    return str.lower()
+
+
+def title(str):
+    return str.title()
+
+
 def create_parser():
     """Creates and returns an argparse cmd line option parser"""
-    parser = argparse.ArgumentParser(description="Perform transformation on input text.")
-    parser.add_argument("-u","--upper", help="convert text to uppercase",action="store_true")
-    parser.add_argument("-l","--lower", help="convert text to lowercase",action="store_true")
-    parser.add_argument("-t","--title", help="convert text to titlecase",action="store_true")
-    parser.add_argument("text", help="text to be manipulated")
+    parser = argparse.ArgumentParser(
+        description='Perform transformation on input text.')
+    parser.add_argument('text', help='text to be manipulated')
+    parser.add_argument(
+        '-u', '--upper', help='convert text to uppercase', action='store_true')
+    parser.add_argument(
+        '-l', '--lower', help='convert text to lowercase', action='store_true')
+    parser.add_argument(
+        '-t', '--title', help='convert text to titlecase', action='store_true')
     return parser
+
 
 def main(args):
     """Implementation of echo"""
-    parser = create_parser()
-    ns = parser.parse_args(args)
 
-    text = ns.text
-    if ns.lower:
-        text = text.lower()
-    
-    if ns.upper:
-        text = text.upper()
+    if not args:
+        parser.print_usage()
+        sys.exit(1)
+    text = args.text
+    if args.upper and args.lower and args.title:
+        text = upper(text)
+        text = lower(text)
+        text = title(text)
+    elif args.lower and args.title:
+        text = lower(text)
+        text = title(text)
+    elif args.upper and args.title:
+        text = upper(text)
+        text = title(text)
+    elif args.upper and args.lower:
+        text = upper(text)
+        text = lower(text)
+    elif args.upper:
+        text = upper(text)
+    elif args.lower:
+        text = lower(text)
+    elif args.title:
+        text = title(text)
+    print(text)
 
-    if ns.title:
-        text = text.title()
-
-    return text
 
 if __name__ == '__main__':
-    print(main(sys.argv[1:]))
+    parser = create_parser()
+    args = parser.parse_args()
+    main(args)
--- tests/test_echo.py
+++ tests/test_echo.py
@@ -1,39 +1,66 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import unittest
 import echo
+import subprocess
+
 
 # Your test case class goes here
+"""Created by ZacharyKline with help from Peter to finish
+    off the finite details"""
 
 
-def test_upper(self):
-    parser = echo.create_parser()
-    args = parser.parse_args(['-u', 'howdy'])
-    print(args)
-    result = echo.upper("howdy".upper())
-    self.assertEquals(result, "HOWDY")
-    self.assertEquals(args.upper, True)
+class TestEcho(unittest.TestCase):
+    def test_upper(self):
+        parser = echo.create_parser()
+        args = parser.parse_args(['-u', 'hello'])
+        print(args)
+        result = echo.upper('hello'.upper())
+        self.assertEquals(result, 'HELLO')
+        self.assertEquals(args.upper, True)
 
+    def test_lower(self):
+        parser = echo.create_parser()
+        args = parser.parse_args(['-l', 'hello'])
+        print(args)
+        result = echo.lower('HELLO'.lower())
+        self.assertEquals(result, 'hello')
+        self.assertEquals(args.lower, True)
 
-def test_lower(self):
-    parser = echo.create_parser()
-    args = parser.parse_args(['-l', 'howdy'])
-    print(args)
-    result = echo.lower("howdy".lower())
-    self.assertEquals(result, "howdy")
-    self.assertEquals(args.lower, True)
+    def test_title(self):
+        parser = echo.create_parser()
+        args = parser.parse_args(['-t', 'hello'])
+        print(args)
+        result = echo.title('hello'.title())
+        self.assertEquals(result, 'Hello')
+        self.assertAlmostEquals(args.title, True)
 
+    def test_help(self):
+        """ Running the program without arguments should show usage. """
 
-def test_title(self):
-    parser = echo.create_parser()
-    args = parser.parse_args(['-t', 'Howdy'])
-    print(args)
-    result = echo.title("Howdy".title())
-    self.assertEquals(result, "Howdy")
-    self.assertEquals(args.title, True)
+        # Run the command `python ./echo.py -h` in a separate process, then
+        # collect it's output.
+        process = subprocess.Popen(
+            ["python", "./echo.py", "-h"],
+            stdout=subprocess.PIPE)
+        stdout, _ = process.communicate()
+        usage = open("./USAGE", "r").read()
+
+        self.assertEquals(stdout, usage)
+
+    def text_all(self):
+        result = echo.upper('hello')
+        result = echo.lower(result)
+        result = echo.title(result)
+
+        self.assertEquals(result, 'Hello')
+
+    def text_none(self):
+        parser = echo.create_parser()
+        args = parser.parse_args(['hello'])
+        result = echo.main(args)
+        self.assertEquals(result, 'hello')
 
 
 if __name__ == '__main__':
-    unittest.main()#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+    unittest.main()
